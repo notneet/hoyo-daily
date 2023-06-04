e49ac5a523cf9ce27e3a5f1d6f44e1391ea1b2d8
アニップ <Hanivan@users.noreply.github.com>
2023-06-04 12:25:33 +0700

    fix: add module scheduler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PlaywrightModule } from '@libs/commons/playwright/playwright.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PlaywrightModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    PlaywrightModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
